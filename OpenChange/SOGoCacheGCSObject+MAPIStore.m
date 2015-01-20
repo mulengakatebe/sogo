f8c95314afc7f5cffd0d7107a2c5b9d898511ca8
Jesús García Sáez <jgarcia@zentyal.com>
2015-01-20 12:10:07 +0100

    Revert "Another fix forgotten in the last commit"
    This reverts commit 3e4d8169734e48e8076a2ae18bd54a97891b20a6.

--- OpenChange/SOGoCacheGCSObject+MAPIStore.m
+++ OpenChange/SOGoCacheGCSObject+MAPIStore.m
@@ -32,7 +32,6 @@
 {
   NSString *className, *mapiMsgClass;
 
-  className = NULL;
   switch (objectType)
     {
     case MAPIMessageCacheObject:
@@ -61,11 +60,6 @@
       [NSException raise: @"MAPIStoreIOException"
                   format: @"message class should not be queried for objects"
                    @" of type '%d'", objectType];
-    }
-    if (className == NULL) {
-        [NSException raise: @"MAPIStoreIOException"
-                    format: @"message class should not be queried for objects"
-                     @" of type '%d'", objectType];
     }
 
   return NSClassFromString (className);
